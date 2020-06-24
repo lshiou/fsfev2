7aa1b6db40d08ab85feb5b14e57327904f6e4981
Lawrence Shiou <hshiou@proofpoint.com>
2020-06-23 17:58:02 -0700

    add custom path

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World from node JS!'));
 
+
+app.get('/demo', (req, res) => {
+    res.set('X-tea-drink', Date.now);
+    res.status(418);
+    return res.send('i am a teapot');
+});
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
