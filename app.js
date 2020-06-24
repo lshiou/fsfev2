5398d8a7ed1fe1205d7ed198da42bde4f4274f06
Lawrence Shiou <hshiou@proofpoint.com>
2020-06-23 18:16:17 -0700

    use proper date value in response header

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('Hello World from node JS!'));
 
 
 app.get('/demo', (req, res) => {
-    res.set('X-tea-drink', Date.now);
+    res.set('X-tea-drink', Date.now());
     res.status(418);
     return res.send('i am a teapot');
 });
